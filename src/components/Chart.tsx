5866b987c3e2918215ad3ae4bf2000e2037fe5f9
khanloo <m.nadirkhanloo1380@gmail.com>
2024-11-10 23:46:17 +0330

    Feat : chart component

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,11 +1,15 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../Redux/store";
+
 function Chart() {
   return (
     <div className="flex items-center justify-center gap-x-10">
       <div>
-        <RadialProgress value={70} primary={true} title="Done" />
+        <RadialProgress typeChart={true} primary={true} title="Done" />
       </div>
-      <div className="">
-        <RadialProgress value={40} primary={false} title="NotDone" />
+      <div>
+        <RadialProgress primary={false} title="NotDone" />
       </div>
     </div>
   );
@@ -13,28 +17,47 @@ function Chart() {
 
 export default Chart;
 
-import React from "react";
-
 interface RadialProgressProps {
-  value: number;
   primary: boolean;
-  title:string
+  title: string;
+  typeChart?: boolean;
 }
 
-const RadialProgress: React.FC<RadialProgressProps> = ({ value, primary,title }) => {
+const RadialProgress: React.FC<RadialProgressProps> = ({
+  primary,
+  title,
+  typeChart,
+}) => {
+  const count = useSelector((state: RootState) => state.todoList);
+  const completeItem = count.filter((item) => {
+    return item.complete === true;
+  }).length;
+
+  let percentageDone = 0;
+  let percentageNotDone = 0;
+
+  if (count.length) {
+    percentageDone = +((completeItem / count.length) * 100).toFixed(0);
+    percentageNotDone = Math.abs(
+      +(((completeItem - count.length) / count.length) * 100).toFixed(0)
+    );
+  }
+  const value = typeChart ? percentageDone : percentageNotDone;
+
   return (
     <>
-    
-    <div
-      className={`radial-progress  text-primary-content  border-4 ${
-        primary ? "border-primary bg-primary" : "bg-secondary border-secondary"
-      }`}
-      style={{ "--value": value } as React.CSSProperties}
-      role="progressbar"
-    >
-      {value}%
-    </div>
-    <p className="text-center mt-3 text-lg">{title}</p>
+      <div
+        className={`radial-progress  text-primary-content  border-4 ${
+          primary
+            ? "border-primary bg-primary"
+            : "bg-secondary border-secondary"
+        }`}
+        style={{ "--value": value } as React.CSSProperties}
+        role="progressbar"
+      >
+        {typeChart ? `${percentageDone}%` : `${percentageNotDone}%`}
+      </div>
+      <p className="text-center mt-3 text-lg">{title}</p>
     </>
   );
 };
